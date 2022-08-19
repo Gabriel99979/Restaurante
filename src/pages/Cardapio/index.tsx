c1c3fb2d28bfd82aedd97f35461e140c438fdc55
Gabriel Amaral <gabriel99979@gmail.com>
2022-08-19 09:47:08 -0300

    Transformando imagem .svg em ReactComponent

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,10 @@
 import styles from "./Cardapio.module.scss"
-
+import {ReactComponent as Logo} from "../../assets/logo.svg"
 export default function Cardapio() {
     return (
         <main>
             <nav className={styles.menu}>
-                <img src="../../assets/logo.sgv" alt="logo aluroni" />
+            <Logo />
             </nav>
         </main>
     );
