f308dc793250ac6a6b3b43d14121d218a1918a33
Gabriel Amaral <gabriel99979@gmail.com>
2022-08-23 18:02:22 -0300

    Adicionando filtro de cor nas opções de prato

--- src/pages/Cardapio/Itens/item/index.tsx
+++ src/pages/Cardapio/Itens/item/index.tsx
@@ -1,15 +1,16 @@
-import logo from 'assets/logo.svg';
 import styles from "./Item.module.scss";
 import cardapio from '../itens.json';
+import classNames from 'classnames'
+
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props){
-    const { title, description, category, size, serving, price } = props;
+    const { title, description, category, size, serving, price, photo } = props;
     return (
        <div className={styles.item}>
         <div className={styles.item__imagem}>
-           <img src={logo} alt={title} />
+           <img src={photo} alt={title} />
         </div>
        
        <div className={styles.item__descricao}>
@@ -18,14 +19,18 @@ export default function Item(props: Props){
                 <p> {description}</p>    
              </div>
              <div className={styles.item__tags}>
-                <div className={styles.item__tipo}>
+                <div className={classNames(
+                    styles.item__tipo,
+                    styles[`item__tipo__${category.label.toLowerCase()}`]
+
+                )}>
                     {category.label}
                 </div>
                 <div className={styles.item__porcao}>
                     {size}g
                 </div>
                 <div className={styles.item__qtdpessoas}>
-                    Serve {serving} pessoa {serving === 1 ? "" : "s"}
+                    Serve {serving} pessoa{serving === 1 ? "" : "s"}
                 </div>
                 <div className={styles.item__valor}>
                     R$ {price.toFixed(2)}
